45003cef3c552822433be6525bed65c7c47ea2d2
Gauch Kirill <kgauch@mail.ru>
2023-10-12 00:42:52 +0300

    complited queue

--- home_work/task_3/main.c
+++ home_work/task_3/main.c
@@ -1,5 +1,4 @@
 #include <unistd.h>
-#include <sys/mman.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
@@ -7,12 +6,20 @@
 #include <stdlib.h>
 #include <sys/time.h>
 #include <wait.h>
+#include <sys/msg.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+#include <semaphore.h>
 
-#define FIFO_NAME "fifo_stream"
+#define SHM
 
 #define BUF_SZ 4096
 
+#if defined(FIFO)
 
+#define FIFO_NAME "fifo_stream"
 
 int fifo_way(FILE* input, FILE* output)
 {
@@ -59,6 +66,102 @@ int fifo_way(FILE* input, FILE* output)
     }
 }
 
+#elif defined(QUEUE)
+
+#define KEY 10
+#define TYPE 1
+
+typedef struct msgbuf {
+    long    mtype;
+    char    mtext[BUF_SZ];
+} message_buf;
+
+int queue_way(FILE* input, FILE* output)
+{
+    int msqid;
+    key_t key = KEY;
+    message_buf msg_buf;
+    msg_buf.mtype = TYPE;
+
+    pid_t pid = fork();
+
+    if (pid < 0)
+    {
+        perror("fork()");
+        return 1;
+    }
+    else
+    if (pid)
+    {
+        if ((msqid = msgget(key, IPC_CREAT | 0666)) < 0) {
+            perror("msgget");
+            exit(1);
+        }
+
+        size_t cnt_read = 0;
+        while(cnt_read = fread(msg_buf.mtext, sizeof(char), BUF_SZ - 1, input)) {
+            msg_buf.mtext[cnt_read] = '\0';
+            size_t buf_len = cnt_read + 1;
+            msgsnd(msqid, &msg_buf, buf_len, 0);
+        }
+
+        strcpy(msg_buf.mtext, "q");
+        size_t buf_len = strlen(msg_buf.mtext) + 1;
+        msgsnd(msqid, &msg_buf, buf_len, 0);
+
+        int status;
+        waitpid(pid, &status, 0);
+        if (!WEXITSTATUS(status))
+            return 3;
+        return 0;
+    }
+    else
+    {
+        if ((msqid = msgget(key, 0666)) < 0) {
+            perror("msgget");
+            exit(1);
+        }
+
+        while(1) {
+            if (msgrcv(msqid, &msg_buf, BUF_SZ, TYPE, 0) < 0) {
+                continue;
+            }
+            msg_buf.mtext[BUF_SZ - 1] = '\0';
+            if (strcmp(msg_buf.mtext, "q") == 0) {
+                break;
+            }
+            size_t len = strlen(msg_buf.mtext);
+            fwrite(msg_buf.mtext, sizeof(char), len, output);
+        }
+        return 0;
+    }
+    
+}
+
+#elif defined(SHM)
+
+int shm_way(FILE* input, FILE* output)
+{
+    sem_t *sem;
+    pid_t pid = fork();
+
+    if (pid < 0)
+    {
+        perror("fork()");
+        return 1;
+    }
+    if (pid)
+    {
+
+    }
+    else
+    {
+        
+    }
+
+}
+
+#endif
 
 
 int main(int argc, char** argv)
@@ -73,21 +176,28 @@ int main(int argc, char** argv)
     
     if (!input || ! output) return 0;
 
-    mknod(FIFO_NAME, S_IFIFO | 0666, 0);
+    #if defined(FIFO)
 
+        mknod(FIFO_NAME, S_IFIFO | 0666, 0);
+
+    #elif defined(QUEUE)
+
+    #elif defined(SHM)
+
+    #endif
 
     struct timeval tv1, tv2, dtv;
 
     gettimeofday(&tv1, NULL);
 
-    #if defined(SHM)
-        if (shm_way(input, output))
+    #if defined(FIFO)
+        if (fifo_way(input, output))
             return 0;
     #elif defined(QUEUE)
         if (queue_way(input, output))
             return 0;
-    #elif defined(FIFO)
-        if (fifo_way(input, output))
+    #elif defined(SHM)
+        if (shm_way(input, output))
             return 0;
     #endif
 
@@ -96,12 +206,12 @@ int main(int argc, char** argv)
     fclose(input);
     fclose(output);
 
-    #ifdef defined(SHM)
-        printf("map ran: ");
+    #if  defined(FIFO)
+        printf("fifo ran: ");
     #elif defined(QUEUE)
         printf("queue ran: ");
-    #elif defined(FIFO)
-        printf("fifo ran: ");
+    #elif defined(SHM)
+        printf("map ran: ");
     #endif
 
     dtv.tv_usec = tv2.tv_usec - tv1.tv_usec;
