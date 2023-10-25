e0d3e001caa69a5799af3fc3bd492cd24eb9622a
Gauch Kirill <kgauch@mail.ru>
2023-10-25 12:25:09 +0300

    started

--- home_work/signal.c
+++ home_work/signal.c
@@ -3,46 +3,35 @@
 #include <stdio.h>
 #include <sys/types.h>
 
-FILE *input = NULL;
-FILE *output = NULL;
+#define BUF_SZ
 
 void sig_handler(int signo, siginfo_t *info, void *context)
 {
-    switch (signo)
-    {
-    case :
-        /* code */
-        break;
-    
-    default:
-        break;
-    }
-}
-
-void sender(int child_pid)
-{
-    struct sigaction sa;
-    sa.sa_flags = SA_SIGINFO;
-    sa.sa_sigaction = sig_handler;
-
-    while (1)
-    {
-        pause();
-    }
-    
     return;
 }
 
-void resiver(int parent_pid)
+void sender(int pid, const char* input_file)
 {
-    while (1)
+    char data[BUF_SZ] = "";
+    FILE* input = fopen(input_file, "rb");
+    if (input)
     {
-        pause();
+        int cnt_read = 0;
+        while ((cnt_read = fread(data, sizeof(int), BUF_SZ, input)) > 0)
+        {
+            /* code */
+        }
+        
+        close(input);
     }
-    return;
 }
 
-int main(int argc, char** argv)
+void resiver(int pid, const char* output_file)
+{
+
+}
+
+int main(int argc, const char** argv)
 {
     if (argc != 3)
     {
@@ -50,19 +39,15 @@ int main(int argc, char** argv)
         return 0;
     }
 
-    input = fopen(argv[1], "r");
-    output = fopen(argv[2], "w");
     pid_t pid = fork();
 
     if (pid < 0)
         perror("fork()");
     else
     if (pid)
-        sender();
+        sender(pid, argv[1]);
     else
-        resiver();
+        resiver(getppid(), argv[2]);
 
-    close(input);
-    close(output);
     return 0;
 }
