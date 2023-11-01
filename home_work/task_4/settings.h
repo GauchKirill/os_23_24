87c2de958554931f0e5d4bd12a115e1c5d411b3f
Gauch Kirill <kgauch@mail.ru>
2023-11-01 14:14:15 +0300

    add data

--- home_work/task_4/settings.h
+++ home_work/task_4/settings.h
@@ -1,8 +1,8 @@
 #ifndef SETTINGS_H
 #define SETTINGS_H
 
-static const long long unsigned CNT_POINTS          = 100000000;
-static const long long unsigned THREADS_ON_SIDE_X   = 1;
+static const long long unsigned CNT_POINTS          = 1000000000;
+static const long long unsigned THREADS_ON_SIDE_X   = 2;
 static const long long unsigned THREADS_ON_SIDE_Y   = 1;
 
 
