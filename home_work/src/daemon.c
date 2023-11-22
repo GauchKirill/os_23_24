eddf51507fa6bfe3bb41bf6660b9379cd275f663
Gauch Kirill <kgauch@mail.ru>
2023-11-22 12:27:21 +0300

    started do daemon

--- home_work/src/daemon.c
+++ home_work/src/daemon.c
@@ -7,6 +7,12 @@
 #include <sys/types.h>
 #include <errno.h>
 #include <sys/stat.h>
+#include <signal.h>
+
+int settings_process(int);
+
+static unsigned period = 1000;
+static unsigned exit_val = 0;
 
 enum modes
 {
@@ -84,7 +90,77 @@ int daemon(int argc, const char** argv)
     return;
 }
 
+void handler(int signo, siginfo_t *info, void *context)
+{
+    switch(signo)
+    {
+        case SIGUSR1:
+            period = info->si_value.sigval_int;
+            break;
+        case SIGUSR2:
+            exit_val = 1;
+            break;
+    }
+    return;
+}
+
 int MonitorProc(int pid, const char* work_dir)
 {
+    int new_pid = fork();
+
+    if (new_pid < 0)
+    {
+        perror("fork");
+        return -1;
+    }
+
+    if (new_pid)
+        return settings_process(new_pid);
     
+    union sigval val;
+    struct sigaction sa;
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = handler;
+    sigaction(SIGUSR1, &sa, NULL);
+    sigaction(SIGUSR2, &sa, NULL);
+
+    while (1)
+    {
+        sleep(period);
+        if (exit_val)
+        {
+
+        }
+
+    }
+}
+
+int settings_process(int child_pid)
+{
+    FILE* settigs_file = fopen("daemon.cfg", "r+");
+    if (!settigs_file)
+    {
+        kill(child_pid, SIGKILL);
+        return -1;
+    }
+
+    union sigval val;
+    struct sigaction sa;
+    sa.sa_flags = SA_SIGINFO;
+
+    while (1)
+    {
+        while (fread(&period, sizeof(int), 1, settigs_file) == 0);
+        
+        if (period == 0)
+        {
+            kill(child_pid, SIGUSR2);
+            return 0;
+        }
+        else
+        {            
+            val.sival_int = period;
+            sigqueue(child_pid, SIGUSR1, val);
+        }
+    }
 }
